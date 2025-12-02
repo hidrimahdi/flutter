475b99461002254c1fe9d9ccca318421ab4c2d31
Country Explorer Dev <dev@example.com>
2025-12-02 06:06:24 -1100

    fix(web): proxy ApiCountries requests via allorigins to avoid CORS

--- lib/services/country_service.dart
+++ lib/services/country_service.dart
@@ -1,13 +1,22 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import '../models/country.dart';
 
 class CountryService {
   static const String baseUrl = 'https://www.apicountries.com';
 
+  Uri _proxied(Uri uri) {
+    if (kIsWeb) {
+      final wrapped = Uri.parse('https://api.allorigins.win/raw?url=${Uri.encodeComponent(uri.toString())}');
+      return wrapped;
+    }
+    return uri;
+  }
+
   Future<List<Country>> getAllCountries() async {
     final uri = Uri.parse('$baseUrl/countries');
-    final res = await http.get(uri);
+    final res = await http.get(_proxied(uri)).timeout(const Duration(seconds: 20));
     if (res.statusCode == 200) {
       final data = json.decode(res.body) as List;
       return data.map((e) => Country.fromJson(e as Map<String, dynamic>)).toList();
@@ -17,7 +26,7 @@ class CountryService {
 
   Future<List<Country>> searchByName(String name) async {
     final uri = Uri.parse('$baseUrl/name/$name');
-    final res = await http.get(uri);
+    final res = await http.get(_proxied(uri)).timeout(const Duration(seconds: 20));
     if (res.statusCode == 200) {
       final data = json.decode(res.body) as List;
       return data.map((e) => Country.fromJson(e as Map<String, dynamic>)).toList();
@@ -27,7 +36,7 @@ class CountryService {
 
   Future<List<Country>> getBorders(String name) async {
     final uri = Uri.parse('$baseUrl/borders/$name');
-    final res = await http.get(uri);
+    final res = await http.get(_proxied(uri)).timeout(const Duration(seconds: 20));
     if (res.statusCode == 200) {
       final data = json.decode(res.body) as List;
       return data.map((e) => Country.fromJson(e as Map<String, dynamic>)).toList();
